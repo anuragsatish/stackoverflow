c5b10f6c97fb339dc6ff8592e897fdcf2707f092
Mike Steder <steder@gmail.com>
2014-08-23 10:21:54 -0500

    Initial commit

---
+++ python-logging-setup-for-multiple-child-log-files-while-disabling-main-log.py
@@ -0,0 +1,105 @@
+import logging
+import sys
+
+
+DEFAULT_LOG_FORMAT = "%(asctime)s [%(levelname)s]: %(message)s"
+DEFAULT_LOG_LEVEL = logging.INFO
+
+
+class DynamicHandlerLogger(logging.Logger):
+    selector = "server"
+
+    def __init__(self, name, level=logging.NOTSET, handler_factory=None):
+        super(DynamicHandlerLogger, self).__init__(name, level=level)
+        self.handler_factory = handler_factory
+
+    def handle(self, record):
+
+        selector = getattr(record, self.selector, None)
+        if selector:
+            newHandler = self.handler_factory(self.name, selector)
+            self.addHandler(newHandler)
+
+        return super(DynamicHandlerLogger, self).handle(record)
+
+
+def server_filter(record):
+    server = getattr(record, "server", None)
+    return True if server else False
+
+
+class ServerFilter(logging.Filter):
+    def __init__(self, server_name, name=''):
+        super(ServerFilter, self).__init__(name=name)
+        self.server_name = server_name
+
+    def filter(self, record):
+        server = getattr(record, "server", None)
+        if server and server == self.server_name:
+            return True
+        return False
+
+
+class MainFilter(logging.Filter):
+    def filter(self, record):
+        return not server_filter(record)
+
+
+def streaming_handler_factory(name, selector):
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(DEFAULT_LOG_FORMAT)
+    handler.setFormatter(formatter)
+    handler.addFilter(ServerFilter(selector))
+    return handler
+
+
+def file_handler_factory(name, selector):
+    handler = logging.FileHandler(filename="%s.%s.log"%(name, selector), encoding="utf-8", mode="a")
+    formatter = logging.Formatter(DEFAULT_LOG_FORMAT)
+    handler.setFormatter(formatter)
+    handler.addFilter(ServerFilter(selector))
+    return handler
+
+
+TO_FILE = True
+if TO_FILE:
+    logger = DynamicHandlerLogger("my.company", handler_factory=file_handler_factory)
+    logger.setLevel(DEFAULT_LOG_LEVEL)
+    handler = logging.FileHandler(filename="my.company.log", encoding="utf-8", mode="a")
+    handler.addFilter(MainFilter())
+    handler.setLevel(DEFAULT_LOG_LEVEL)
+    formatter = logging.Formatter(DEFAULT_LOG_FORMAT)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+else:
+    logger = DynamicHandlerLogger("my.company", handler_factory=streaming_handler_factory)
+    logger.setLevel(DEFAULT_LOG_LEVEL)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.addFilter(MainFilter())
+    handler.setLevel(DEFAULT_LOG_LEVEL)
+    formatter = logging.Formatter(DEFAULT_LOG_FORMAT)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+def process(server):
+    #logger.info("This should show up only in the server-specific log file for %s", server, extra={"server": server})
+    server_logger = logging.LoggerAdapter(logger, {'server': server})
+    server_logger.info("This should show up only in the server-specific log file for %s", server)
+    server_logger.info("another log message")
+
+
+def main():
+    # servers list retrieved from another function, just here for iteration
+    servers = ["server1", "server2", "server3"]
+
+    logger.info("This should show up in the console and main.log.")
+
+    for server in servers:
+        process(server)
+
+    logger.info("This should show up in the console and main.log again.")
+
+
+if __name__ == "__main__":
+    main()
