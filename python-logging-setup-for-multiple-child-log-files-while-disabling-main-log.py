5a4300fe2518992e45ba64b1f3c7d7751af0b7ee
Mike Steder <steder@gmail.com>
2014-08-23 11:11:42 -0500

    simpler solution use child loggers

--- python-logging-setup-for-multiple-child-log-files-while-disabling-main-log.py
+++ python-logging-setup-for-multiple-child-log-files-while-disabling-main-log.py
@@ -5,57 +5,31 @@ DEFAULT_LOG_FORMAT = "%(asctime)s [%(levelname)s]: %(message)s"
 DEFAULT_LOG_LEVEL = logging.INFO
 
 
-class DynamicHandlerLogger(logging.Logger):
+class HandlerPerChildLogger(logging.Logger):
     selector = "server"
 
-    def __init__(self, name, level=logging.NOTSET, handler_factory=None):
-        super(DynamicHandlerLogger, self).__init__(name, level=level)
+    def __init__(self, name, handler_factory, level=logging.NOTSET):
+        super(HandlerPerChildLogger, self).__init__(name, level=level)
         self.handler_factory = handler_factory
-        self._handler_cache = {}
 
-    def handle(self, record):
-        selector = getattr(record, self.selector, None)
-        if selector and selector not in self._handler_cache:
-            newHandler = self.handler_factory(self.name, selector)
-            self.addHandler(newHandler)
-            self._handler_cache[selector] = newHandler
-        return super(DynamicHandlerLogger, self).handle(record)
+    def getChild(self, suffix):
+        logger = super(HandlerPerChildLogger, self).getChild(suffix)
+        if not logger.handlers:
+            logger.addHandler(self.handler_factory(logger.name))
+            logger.setLevel(DEFAULT_LOG_LEVEL)
+        return logger
 
 
-def server_filter(record):
-    server = getattr(record, "server", None)
-    return True if server else False
-
-
-class ServerFilter(logging.Filter):
-    def __init__(self, server_name, name=''):
-        super(ServerFilter, self).__init__(name=name)
-        self.server_name = server_name
-
-    def filter(self, record):
-        server = getattr(record, "server", None)
-        if server and server == self.server_name:
-            return True
-        return False
-
-
-class MainFilter(logging.Filter):
-    def filter(self, record):
-        return not server_filter(record)
-
-
-def file_handler_factory(name, selector):
-    handler = logging.FileHandler(filename="%s.%s.log"%(name, selector), encoding="utf-8", mode="a")
+def file_handler_factory(name):
+    handler = logging.FileHandler(filename="{}.log".format(name), encoding="utf-8", mode="a")
     formatter = logging.Formatter(DEFAULT_LOG_FORMAT)
     handler.setFormatter(formatter)
-    handler.addFilter(ServerFilter(selector))
     return handler
 
 
-logger = DynamicHandlerLogger("my.company", handler_factory=file_handler_factory)
+logger = HandlerPerChildLogger("my.company", file_handler_factory)
 logger.setLevel(DEFAULT_LOG_LEVEL)
 handler = logging.FileHandler(filename="my.company.log", encoding="utf-8", mode="a")
-handler.addFilter(MainFilter())
 handler.setLevel(DEFAULT_LOG_LEVEL)
 formatter = logging.Formatter(DEFAULT_LOG_FORMAT)
 handler.setFormatter(formatter)
@@ -63,7 +37,7 @@ logger.addHandler(handler)
 
 
 def process(server):
-    server_logger = logging.LoggerAdapter(logger, {'server': server})
+    server_logger = logger.getChild(server)
     server_logger.info("This should show up only in the server-specific log file for %s", server)
     server_logger.info("another log message for %s", server)
 
